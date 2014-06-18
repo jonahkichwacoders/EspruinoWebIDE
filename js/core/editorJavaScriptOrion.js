8245e949dd4429cc87622b80f50e9ebe202ca2a1
Jonah Graham <jonah@kichwacoders.com>
2014-06-18 12:47:46 +0100

    Experiment with templates/code assist

--- js/core/editorJavaScriptOrion.js
+++ js/core/editorJavaScriptOrion.js
@@ -11,15 +11,38 @@
 **/
 "use strict";
 (function(){
-  var orionEditor = null;
+  var editor = null;
   var preloadedContent = null;
 
   function init() {
     $('<div id="divcode" style="width:100%;height:100%;display:none"><pre class="orioneditor" data-editor-lang="js" style="height:95%"></pre></div>').appendTo(".editor--code .editor__canvas");
-    require(["orion/editor/edit"], function(edit) {
+    require(["orion/editor/edit", "orion/editor/templates"], function(edit, templates) {
 
-      orionEditor = edit({className: "orioneditor", lang: "javascript"})[0];
-      orionEditor.setText(preloadedContent);
+      editor = edit({className: "orioneditor", lang: "javascript"})[0];
+      editor.setText(preloadedContent);
+      
+
+      var temps = [
+      {
+        prefix: "SELECT", //$NON-NLS-0$
+        description: "Select table", //$NON-NLS-0$
+        template: "SELECT ${column} FROM ${table_name};" //$NON-NLS-1$ //$NON-NLS-0$
+      },
+      {
+        prefix: "INSERT INTO", //$NON-NLS-0$
+        description: "Insert in table", //$NON-NLS-0$
+        template: "INSERT INTO ${table_name} VALUES (${values});" //$NON-NLS-0$
+      }
+      ];
+      var keywords = [
+        "SELECT", //$NON-NLS-0$
+        "WHERE", //$NON-NLS-0$
+        "WHEN", //$NON-NLS-0$
+        "DELETE" //$NON-NLS-0$
+      ];
+      
+      var contentAssist = editor.getContentAssist();
+      contentAssist.setProviders([new templates.TemplateContentAssist(keywords, temps)]);
 
       // all set up, ready to show the code frame
       $("#divcode").show();
@@ -27,14 +50,14 @@
   }
 
   function getCode() {
-    return orionEditor.getText();
+    return editor.getText();
   }
   
   function setCode(code) {
-    if (orionEditor == null)
+    if (editor == null)
       preloadedContent = code;
     else
-      orionEditor.setText(code);
+      editor.setText(code);
   }
   
   Espruino.Core.EditorJavaScript = {
